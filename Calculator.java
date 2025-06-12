d3c7ed7f921364f274e0f126943aa497b0b8ef36
ociblu <ociblu7@gmail.com>
2025-06-12 20:31:47 +0700

    update

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+public class Calculator {
+    int boardWidth = 360;
+    int boardHeight = 540;
+
+    JFrame frame = new JFrame("Calculator");
+}
