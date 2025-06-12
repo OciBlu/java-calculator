9e942a1567b83cc1c70195d36b8142e4dd013179
ociblu <ociblu7@gmail.com>
2025-06-13 02:26:34 +0700

    update

--- Calculator.java
+++ Calculator.java
@@ -9,4 +9,13 @@ public class Calculator {
     int boardHeight = 540;
 
     JFrame frame = new JFrame("Calculator");
+
+    Calculator(){
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
 }
