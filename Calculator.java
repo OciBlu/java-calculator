cea532bf1c9ee04b37dfbd4b0e91c59652b8aff7
ociblu <ociblu7@gmail.com>
2025-06-14 12:27:03 +0700

    update

--- Calculator.java
+++ Calculator.java
@@ -5,17 +5,48 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 public class Calculator {
+    //Deklarasi variabel ukuran Frame
     int boardWidth = 360;
     int boardHeight = 540;
 
+    //Deklarasi variabel Warna
+    Color customLightGrey = new Color(212, 212, 210);
+    Color customDarkGrey = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+    Color customOrange = new Color(225, 149, 0);
+
+    //Set buttonValue
+    String[] buttonValue = {
+        "AC", "+/-", "%", ":",
+    };
+
+    //Deklarasi variabel Contain & Components
     JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
+    JPanel buttonsPanel = new JPanel();
 
     Calculator(){
+
+        //Set variable frame
         frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        //Set variable displayLabel
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        //Set variable displayPanel
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayPanel, BorderLayout.NORTH);
     }
 }
